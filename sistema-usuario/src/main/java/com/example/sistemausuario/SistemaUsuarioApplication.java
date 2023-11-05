7a843f47a6713bb64700deb1677b50194c39ee14
Izabelle Garcez <izabellegarcez33@gmail.com>
2023-11-05 15:58:34 -0300

    primeiro commit

---
+++ sistema-usuario/src/main/java/com/example/sistemausuario/SistemaUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.example.sistemausuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaUsuarioApplication.class, args);
+	}
+
+}
---
+++ sistema-usuario/src/test/java/com/example/sistemausuario/SistemaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistemausuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
